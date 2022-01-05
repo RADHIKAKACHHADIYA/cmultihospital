4b41b21ca11ea3ea39f6ce15bc9d69e76e8af0de
dhruvit560 <dhruvitpatel560@gmail.com>
2022-01-05 09:16:16 +0530

    demo_done

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -6,7 +6,7 @@ function Home(props) {
             <div>
                 <section id="hero" className="d-flex align-items-center">
                     <div className="container">
-                        <h1>Welcome to City <br />Multispeciality Hospital</h1>
+                        <h1>Welcome to City 1<br />Multispeciality Hospital</h1>
                         <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
                     </div>
                 </section>
